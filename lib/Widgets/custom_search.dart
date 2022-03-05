e678833b531bbe1b2dd0d8c00d4a75a2fd485571
negi-tushar <negitushar44@gmail.com>
2022-03-05 16:41:55 +0530

    comment added

--- lib/Widgets/custom_search.dart
+++ lib/Widgets/custom_search.dart
@@ -42,8 +42,8 @@ class customSearch extends StatelessWidget {
         },
         onSuggestionSelected: (ClinicDetails? suggestion) {
           final details = suggestion!;
-          ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: Text(details.id)));
+          // ScaffoldMessenger.of(context)
+          //     .showSnackBar(SnackBar(content: Text(details.id)));
           Navigator.of(context)
               .pushNamed(ClinicDetailsScreen.id, arguments: details);
         },
